677d0240c29e70c5096abd834ab367425bb7b7a6
sbraunstein42 <sbraunstein@neit.edu>
2025-11-06 07:36:17 -0500

    Initial commit

---
+++ challenges.js
@@ -0,0 +1,30 @@
+let canvas = document.getElementById("myCanvas");
+let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
+
+
+function gameLoop() {
+    
+    //erase the canvas
+    pencil.clearRect(0, 0, canvas.width, canvas.height);
+
+
+
+}
+
+setInterval(gameLoop, 50);
+
+let score = 0;
+
+//score goes up every second
+function raiseScore() {
+    score += 1;
+    let scoreElement = document.getElementById("scoreDisplay");
+    scoreElement.innerHTML = score;
+}
+setInterval(raiseScore, 1000);
+
+function detectClick() {
+    console.log("CLicked!")
+}
+
+canvas.addEventListener("click", detectClick);
